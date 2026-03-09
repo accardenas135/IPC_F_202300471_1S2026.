b0d819cd294d62b720c26ecf00fa494edfefcc2a
anto <antoniocar318@gmail.com>
2026-03-08 20:47:39 -0600

    agregando el commit correcto

---
+++ inventarioTienda/src/inventarioTienda.java
@@ -0,0 +1,8 @@
+public class inventarioTienda {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+                ventanaPrincipal ventana = new ventanaPrincipal();
+                ventana.setVisible(true);
+        });
+    }
+}
---
+++ inventarioTienda/src/ventanaPrincipal.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class ventanaPrincipal extends JFrame {
+    public ventanaPrincipal() {
+        setTitle("INVETARIO DE TIENDA");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
+        // Aquí más adelante (Paso 4 en adelante) añadiremos:
+        // - Panel de entrada de datos
+        // - Tabla de inventario
+        // - Botones de interacción
+
+    }
+}
